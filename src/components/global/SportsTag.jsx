48db007979178e7b27b900eec6163138ceee3e41
Donghwan Oh <imodh97@gmail.com>
2024-05-20 00:52:02 +0900

    Feature: 종목 필터링 수정 및 디테일 필터링 구현 (난이도) - 스포츠 태그 비활성화 시 리랜더 문제 해결 - select에 중첩 함수 이용한 디테일 필터링 기초 구현

--- src/components/global/SportsTag.jsx
+++ src/components/global/SportsTag.jsx
@@ -1,34 +1,53 @@
 import { styled } from "styled-components";
 
 const Tag = styled.div`
-  background-color: ${(props) => props.bgcolor};
-  color: ${(props) => props.color};
+  background-color: ${(props) => props.$bgColor};
+  color: ${(props) => props.$color};
   padding: 4px 8px;
   border-radius: 6px;
-  border: 2px solid ${(props) => props.hovercolor};
+  border: 2px solid ${(props) => props.$hoverColor};
   font-size: var(--font-size-s);
   cursor: pointer;
   &:hover {
-    background-color: ${(props) => props.hovercolor};
+    background-color: ${(props) => props.$hoverColor};
   }
   transition: all 0.2s ease-in-out;
+  opacity: 0.7;
+`;
+
+const ClickedTag = styled(Tag)`
+  opacity: 1;
 `;
 
 export default function SportsTag({
   color,
-  bgcolor,
-  hovercolor,
+  bgColor,
+  hoverColor,
   text,
   onClick,
+  hasClicked,
 }) {
   return (
-    <Tag
-      bgcolor={bgcolor}
-      hovercolor={hovercolor}
-      color={color}
-      onClick={onClick}
-    >
-      {text}
-    </Tag>
+    <>
+      {hasClicked ? (
+        <ClickedTag
+          $bgColor={bgColor}
+          $hoverColor={hoverColor}
+          $color={color}
+          onClick={onClick}
+        >
+          {text}
+        </ClickedTag>
+      ) : (
+        <Tag
+          $bgColor={bgColor}
+          $hoverColor={hoverColor}
+          $color={color}
+          onClick={onClick}
+        >
+          {text}
+        </Tag>
+      )}
+    </>
   );
 }
--- src/pages/program/Program.jsx
+++ src/pages/program/Program.jsx
@@ -78,7 +78,7 @@ const ProgramContainer = styled.div`
 
 /* Filters */
 
-const FilterContainer = styled.div`
+const FilterContainer = styled.form`
   width: 80%;
   height: 400px;
   display: flex;
@@ -100,7 +100,7 @@ const MainFilter = styled.div`
   height: 50px;
   display: flex;
   gap: 10px;
-  padding: 10px 0;
+  padding-bottom: 10px;
   font-weight: bold;
 `;
 
@@ -148,16 +148,13 @@ const DetailFilter = styled.div`
   padding: 20px;
   border: 2px solid var(--color-gray);
   border-radius: 12px;
+  position: relative;
 `;
 
 const FilterRow = styled.div`
   height: 40px;
-  width: 80%;
+  width: 90%;
   display: flex;
-  border-bottom: 1px solid var(--color-gray);
-  &:last-child {
-    border: none;
-  }
 `;
 
 const FilterCol = styled.div`
@@ -167,95 +164,129 @@ const FilterCol = styled.div`
   align-items: center;
 
   &:first-child {
-    flex: 1;
+    flex: 0.6;
     font-weight: bold;
+    display: flex;
+    justify-content: flex-start;
   }
 
   &:nth-child(2) {
     flex: 2;
+    display: flex;
+    justify-content: flex-start;
   }
+
   input {
     margin-right: 10px;
   }
 `;
 
+const FilterSubmit = styled.div`
+  display: flex;
+  gap: 10px;
+  position: absolute;
+  right: 50px;
+  bottom: 30px;
+`;
+
 export default function Program() {
-  /* REACT HOOK FORM */
-  /* search 폼 관리 */
+  /* 검색 기능 */
+  const [searchKeyword, setSearchKeyword] = useState("");
+
   const { register: searchRegister, handleSubmit: handleSearchSubmit } =
     useForm();
 
-  // keyword를 state로 받은 후 useQuery에 매개변수로 전달, api 요청
-  const [searchKeyword, setSearchKeyword] = useState("");
+  // Search button 클릭 시 keyword를 setSearchKeyword에 저장
   const onSearchSubmit = (data) => {
     setSearchKeyword(data.keyword);
   };
 
-  /* REACT QUERY */
-  /* get programs */
-  const [tagIds, setTagIds] = useState([]);
+  /* 프로그램 필터링 1. 스포츠 태그 */
+  const { isLoading: sportsLoading, data: sportsData } = useQuery(
+    ["sports"],
+    getSports
+  );
+
+  const [clickedTags, setClickedTags] = useState([]);
+
+  // 스포츠 태그 클릭 시 clickedTags 배열에서 해당 id 토글
+  const hanldeTagClicked = (id) => {
+    if (clickedTags.includes(id)) {
+      setClickedTags((prev) => prev.filter((it) => it !== id));
+    } else {
+      setClickedTags((prev) => [...prev, id]);
+    }
+  };
+
+  /* 프로그램 필터링 2. 세부 필터 */
+  const [filterDetails, setFilterDetails] = useState({
+    diff: [],
+  });
+
+  const {
+    register: filterRegister,
+    handleSubmit: handleFilterSubmit,
+    reset: filterReset,
+  } = useForm();
+
+  const onFilterValid = (data) => {
+    setFilterDetails(data);
+  };
+
+  // searchKeyword가 적용된 programsData에서 스포츠 태그, 디테일 필터링 적용 후 select
+  const filterProgramBySports = (programs) => {
+    return clickedTags.length === 0
+      ? programs
+      : programs.filter((program) => clickedTags.includes(program.sport_id));
+  };
+
+  const filterProgramByDetails = (programs) => {
+    console.log(JSON.stringify(filterDetails));
+    return filterDetails.diff.length === 0
+      ? programs
+      : programs.filter((program) =>
+          filterDetails.diff.includes(program.difficulty)
+        );
+  };
 
   const { isLoading: programsLoading, data: programsData } = useQuery(
     ["programs", searchKeyword],
     () => getPrograms(searchKeyword),
     {
       select: (programsData) =>
-        tagIds.length === 0
-          ? programsData
-          : programsData.filter((program) => {
-              return tagIds.includes(program.id);
-            }),
+        filterProgramByDetails(filterProgramBySports(programsData)),
     }
   );
-
-  /* get sports */
-  const { isLoading: sportsLoading, data: sportsData } = useQuery(
-    ["sports"],
-    getSports
-  );
-
-  // id가 tagIds에 존재하면 제거, 존재하지 않으면 추가
-  const hanldeTagClicked = (id) => {
-    if (tagIds.includes(id)) {
-      const idx = tagIds.indexOf(id);
-      setTagIds((prev) => {
-        prev.splice(idx, 1);
-        return prev;
-      });
-    } else {
-      setTagIds((prev) => [...prev, id]);
-    }
-  };
-
   return (
     <Body>
       <Wrapper>
         <SearchContainer onSubmit={handleSearchSubmit(onSearchSubmit)}>
           <Input
-            placeholder="키워드를 입력하세요"
+            placeholder="키워드를 입력하세요 (공백 입력 시 모든 프로그램 출력)"
             {...searchRegister("keyword")}
           />
           <SearchButton>검색</SearchButton>
         </SearchContainer>
-        <FilterContainer>
-          <SportsFilter>
-            <h3>종목</h3>
-            {sportsLoading
-              ? "Loading..."
-              : sportsData.map((sport) => {
-                  return (
-                    <SportsTag
-                      key={sport.id}
-                      color={"#ff4d4d"}
-                      text={sport.title}
-                      bgcolor={"#ffcccc"}
-                      hovercolor={"#ffb8b8"}
-                      // handleTagClicked 함수를 onClick props로 전달함
-                      onClick={() => hanldeTagClicked(sport.id)}
-                    />
-                  );
-                })}
-          </SportsFilter>
+        <SportsFilter>
+          <h3>종목</h3>
+          {sportsLoading
+            ? "Loading..."
+            : sportsData.map((sport) => {
+                return (
+                  <SportsTag
+                    key={sport.id}
+                    color={"#ff4d4d"}
+                    text={sport.title}
+                    bgColor={"#ffcccc"}
+                    hoverColor={"#ffb8b8"}
+                    // handleTagClicked 함수를 onClick props로 전달
+                    onClick={() => hanldeTagClicked(sport.id)}
+                    hasClicked={clickedTags.includes(sport.id)}
+                  />
+                );
+              })}
+        </SportsFilter>
+        <FilterContainer onSubmit={handleFilterSubmit(onFilterValid)}>
           <MainFilter>
             <DateAndTime>
               <SmallFilterBox>날짜</SmallFilterBox>
@@ -271,11 +302,29 @@ export default function Program() {
               </FilterCol>
               <FilterCol>
                 <label htmlFor="high">상</label>
-                <input id="high" name="difficulty" type="checkbox" />
+                <input
+                  id="high"
+                  name="difficulty"
+                  type="checkbox"
+                  value="어려움"
+                  {...filterRegister("diff")}
+                />
                 <label htmlFor="mid">중</label>
-                <input id="mid" name="difficulty" type="checkbox" />
+                <input
+                  id="mid"
+                  name="difficulty"
+                  type="checkbox"
+                  value="보통"
+                  {...filterRegister("diff")}
+                />
                 <label htmlFor="low">하</label>
-                <input id="low" name="difficulty" type="checkbox" />
+                <input
+                  id="low"
+                  name="difficulty"
+                  type="checkbox"
+                  value="쉬움"
+                  {...filterRegister("diff")}
+                />
               </FilterCol>
             </FilterRow>
             <FilterRow>
@@ -283,8 +332,10 @@ export default function Program() {
                 <span>예약방식</span>
               </FilterCol>
               <FilterCol>
-                <select>
-                  <option>Booking</option>
+                <select {...filterRegister("booking")}>
+                  <option value="call">전화 예약</option>
+                  <option value="homeapge">홈페이지 예약</option>
+                  <option value="waybu">웨이부에서 예약</option>
                 </select>
               </FilterCol>
             </FilterRow>
@@ -294,13 +345,33 @@ export default function Program() {
               </FilterCol>
               <FilterCol>
                 <label htmlFor="ten">10대</label>
-                <input id="ten" type="checkbox" />
+                <input
+                  id="ten"
+                  type="checkbox"
+                  {...filterRegister("age")}
+                  value="10"
+                />
                 <label htmlFor="twenty">20대</label>
-                <input id="twenty" type="checkbox" />
+                <input
+                  id="twenty"
+                  type="checkbox"
+                  {...filterRegister("age")}
+                  value="20"
+                />
                 <label htmlFor="thirty">30대</label>
-                <input id="thirty" type="checkbox" />
+                <input
+                  id="thirty"
+                  type="checkbox"
+                  {...filterRegister("age")}
+                  value="30"
+                />
                 <label htmlFor="elder">40대 이상</label>
-                <input id="elder" type="checkbox" />
+                <input
+                  id="elder"
+                  type="checkbox"
+                  {...filterRegister("age")}
+                  value="40"
+                />
               </FilterCol>
             </FilterRow>
             <FilterRow>
@@ -308,7 +379,12 @@ export default function Program() {
                 <span>인원</span>
               </FilterCol>
               <FilterCol>
-                <input style={{ width: "40px" }} type="number" />
+                <input
+                  min={0}
+                  style={{ width: "40px" }}
+                  type="number"
+                  {...filterRegister("max")}
+                />
               </FilterCol>
             </FilterRow>
             <FilterRow>
@@ -316,11 +392,23 @@ export default function Program() {
                 <span>프로그램 기간</span>
               </FilterCol>
               <FilterCol>
-                <input type="date" />
+                <input type="date" {...filterRegister("start")} />
                 <span style={{ marginRight: "10px" }}>~</span>
-                <input type="date" />
+                <input type="date" {...filterRegister("end")} />
               </FilterCol>
             </FilterRow>
+            <FilterSubmit>
+              <input type="submit" value="필터 적용" />
+              <div
+                onClick={() => {
+                  setClickedTags([]);
+                  setFilterDetails({ diff: [] });
+                  filterReset();
+                }}
+              >
+                초기화
+              </div>
+            </FilterSubmit>
           </DetailFilter>
         </FilterContainer>
         <ProgramContainer>
