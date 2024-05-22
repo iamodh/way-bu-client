eccce5d7b70dd97ee8112472018d40edf1ea2135
Oh-seo-jin <seojin2414@gmail.com>
2024-05-23 08:44:31 +0900

    Feat: 헤더에 로고 추가

--- src/components/layout/CommonLayout.jsx
+++ src/components/layout/CommonLayout.jsx
@@ -8,52 +8,88 @@ const Header = styled.header`
   box-shadow: 0px 5px 10px 0px lightgray;
 `;
 const Sign = styled.div`
+  grid-row-start: 1;
   display: flex;
   flex-direction: row;
-  justify-content: flex-end;
+  justify-content: space-between;
+  align-items: center;
   font-size: var(--font-size-m);
 `;
 const Logo = styled.img`
-  width: 140px;
-  height: 40px;
-  background-color: var(--color-blue-main);
+  width: 120px;
+  object-fit: cover;
+  margin: var(--padding-3xs) var(--padding-13xl);
+  @media screen and (max-width: 768px) {
+    width: 80px;
+    margin: var(--padding-9xs) var(--padding-3xs);
+  }
+`;
+const Div = styled.div`
+  flex: 1;
+`;
+const SignItem = styled.div`
+  padding: var(--padding-9xs) var(--padding-xs);
+  cursor: pointer;
+  font-size: inherit;
+`;
+const Search = styled.div``;
+const SearchInput = styled.input`
+  display: none;
 `;
 
-const Menubar = styled.ul`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
+const SearchButton = styled.button`
+  width: var(--font-size-m);
+  height: var(--font-size-m);
+  background-image: url("/icon/search.svg");
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-color: transparent;
+  border: none;
+  margin: 0 var(--padding-5xs);
 `;
-
-const Nav = styled.ul`
-  width: 30%;
+const Alarm = styled.button`
+  width: var(--font-size-m);
+  height: var(--font-size-m);
+  background-image: url("/icon/alarm.svg");
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-color: transparent;
+  border: none;
+  margin: 0 var(--padding-5xs);
+`;
+const ProfileImage = styled.img`
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  margin: var(--padding-5xs) 64px var(--padding-5xs);
+  @media screen and (max-width: 768px) {
+    width: 40px;
+    height: 40px;
+    margin: var(--padding-9xs) var(--padding-xs) var(--padding-9xs);
+  }
+`;
+const Nav = styled.div`
+  max-width: 1000px;
+  width: 70%;
+  margin: 0 auto;
+  padding: 10px 0;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
+  @media screen and (max-width: 768px) {
+    /* background-color: salmon; */
+    width: 100%;
+    padding: var(--padding-9xs) var(--padding-3xs);
+  }
 `;
-const NavItem = styled.li`
-  border: 2px solid salmon;
-
+const NavItem = styled.div`
   padding: var(--padding-9xs) var(--padding-xl);
-  list-style: none;
   cursor: pointer;
 `;
 const StyledLink = styled(Link)`
   text-decoration: none;
   color: black;
-`;
-const Search = styled.div``;
-const SearchInput = styled.input``;
-
-const SearchButton = styled.button``;
-const Alarm = styled.button``;
-const ProfileImage = styled.img`
-  width: 64px;
-  height: 64px;
-  border-radius: 50%;
-  margin-right: 64px;
 `;
 
 export default function CommonLayout() {
@@ -61,31 +97,16 @@ export default function CommonLayout() {
     <>
       <Header>
         <Sign>
+          <StyledLink to={"/"}>
+            <Logo src="/img/logo.png" />
+          </StyledLink>
+          <Div />
           <StyledLink to={"/login"}>
-            <NavItem>로그인</NavItem>
+            <SignItem>로그인</SignItem>
           </StyledLink>
           <StyledLink to={"/signup"}>
-            <NavItem>회원가입</NavItem>
+            <SignItem>회원가입</SignItem>
           </StyledLink>
-        </Sign>
-        <Menubar>
-          <StyledLink to={"/"}>
-            <Logo />
-          </StyledLink>
-          <Nav>
-            <StyledLink to={"/program"}>
-              <NavItem>프로그램</NavItem>
-            </StyledLink>
-            <StyledLink to={"/sports"}>
-              <NavItem>스포츠</NavItem>
-            </StyledLink>
-            <StyledLink to={"/matcing"}>
-              <NavItem>매칭</NavItem>
-            </StyledLink>
-            <StyledLink to={"/"}>
-              <NavItem>커뮤니티</NavItem>
-            </StyledLink>
-          </Nav>
           <Search>
             <SearchInput type="text" />
             <SearchButton />
@@ -94,7 +115,21 @@ export default function CommonLayout() {
           <StyledLink to="/mypage">
             <ProfileImage src="/img/ellipse-13@2x.png" />
           </StyledLink>
-        </Menubar>
+        </Sign>
+        <Nav>
+          <StyledLink to={"/program"}>
+            <NavItem>프로그램</NavItem>
+          </StyledLink>
+          <StyledLink to={"/sports"}>
+            <NavItem>스포츠</NavItem>
+          </StyledLink>
+          <StyledLink to={"/matcing"}>
+            <NavItem>매칭</NavItem>
+          </StyledLink>
+          <StyledLink to={"/"}>
+            <NavItem>커뮤니티</NavItem>
+          </StyledLink>
+        </Nav>
       </Header>
       <Outlet />
     </>
