ed504256bb6bbd72f18df29633ab9c77b4c2a31e
mongzino <112944480+mongzino@users.noreply.github.com>
2024-05-24 09:37:20 +0900

    Feat. 커뮤니티 게시글 확인 추가 - 게시글 id url parameter로 페이지별 연결 - 게시글 정보 불러오기

--- src/pages/community/Post.jsx
+++ src/pages/community/Post.jsx
@@ -1,1 +1,51 @@
-export default function Post() {}
+import { useParams } from "react-router-dom";
+import { loggedInUserState, loggedInUserProfileState } from "../../atom";
+import { useState, useEffect } from "react";
+import { client } from "../../../libs/supabase";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+
+const PostWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 1000px;
+  margin: 0 auto;
+  flex-direction: column;
+  & * {
+    box-sizing: border-box;
+  }
+`;
+
+export default function Post() {
+  const { id: postId } = useParams();
+
+  const [post, setPost] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getPost = async () => {
+    let { data: post, error } = await client
+      .from("POST")
+      .select(
+        "post_id, title, contents, post_type, user_nickname, user_id, views, thumbs, created_at, updated_at"
+      )
+      .eq("post_id", postId);
+    console.log(post);
+    setPost(post[0]);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  if (isLoading) return <>Loading...</>;
+
+  return (
+    <PostWrapper>
+      <h1>{post.title}</h1>
+      <p>{post.user_nickname}</p>
+      <div>{post.contents}</div>
+    </PostWrapper>
+  );
+}
