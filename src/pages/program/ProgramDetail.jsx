8206dc2f62a0ee205aecbbe1c503bab865824a62
Donghwan Oh <imodh97@gmail.com>
2024-06-19 09:19:55 +0900

    Fix: 스포츠 모달 스크롤 수정

--- src/pages/program/ProgramDetail.jsx
+++ src/pages/program/ProgramDetail.jsx
@@ -7,7 +7,7 @@ export default function ProgramDetail() {
       {program[0].detail_images.length === 0
         ? "아직 없습니다."
         : program[0].detail_images.map((imgUrl) => (
-            <img style={{ width: "100%", margin: "-2px 0" }} src={imgUrl} />
+            <img style={{ width: "100%" }} src={imgUrl} />
           ))}
     </>
   );
